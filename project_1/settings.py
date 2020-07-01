87d92c5ccb3958d8e544b73163561955b7346cc9
Yun Sang Woo <Awarduuu@gmail.com>
2020-07-01 22:17:52 +0900

    static

--- project_1/settings.py
+++ project_1/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'first',
-    'introduction'
+    'introduction',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'project_1', 'static')
+]
