e9df0021212575444da4ef10ba04fe18cbead171
Yun Sang Woo <Awarduuu@gmail.com>
2020-09-22 23:59:35 +0900

    9/22 modelform

--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from django.contrib.auth.decorators import login_required
-
-def new(request):
-    return render(request, 'posts/new.html')
+from .forms import PostForm
 
 
+@login_required
 def create(request):
-    if request.method =="POST":
-        title = request.POST.get('title')
-        user = request.user
-        content = request.POST.get('content')
-        image = request.FILES.get('image')
-        Post.objects.create(title=title, content=content, image=image, user=user)
-        return redirect('posts:main')
+    # if request.method =="POST":
+    #     title = request.POST.get('title')
+    #     user = request.user
+    #     content = request.POST.get('content')
+    #     image = request.FILES.get('image')
+    #     Post.objects.create(title=title, content=content, image=image, user=user)
+    #     return redirect('posts:main')
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save(user = request.user)
+        return redirect('main')
+    else:
+        form = PostForm()
+    return render(request, 'posts/create.html', {'form' : form})
 
 
 def main(request):
@@ -28,14 +35,23 @@ def show(request, id):
     return render(request,'posts/show.html', {'post': post})
 
 def update(request, id):
+   
+#    if request.method =="POST":
+#        post.title = request.POST.get('title')
+#        post.content = request.POST.get('content')
+#        post.image = request.FILES.get('image')
+#        post.save()
+#        return redirect('posts:show', post.id)
+#    return render(request,'posts/update.html', {"post":post})
     post = get_object_or_404(Post, pk=id)
-    if request.method =="POST":
-        post.title = request.POST.get('title')
-        post.content = request.POST.get('content')
-        post.image = request.FILES.get('image')
-        post.save()
-        return redirect('posts:show', post.id)
-    return render(request,'posts/update.html', {"post":post})
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save(user = request.user)
+            return redirect('post:show', post.id)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'posts/update.html', {'form' : form})
 
 def delete(request, id):
     post = get_object_or_404(Post, pk=id)
